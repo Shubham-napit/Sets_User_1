415d3a2051a561c1389c746a468b798ed33f8006
Shubham-napit <ashoknapit7999@gmail.com>
2024-08-29 18:33:24 +0530

    change

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,30 @@
-public class HashSets{
+import java.util.HashSet;
+
+public class HashSets
+{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+
+public static void main(String[] args) 
+{
+    HashSet<Person> set=new HashSet<>();
+
+    set.add(new Person("shubham",21 ));
+    set.add(new Person("vikas", 21));
+    set.add(new Person("atul", 23));
+
+
+    for(Object obj :set)
+    {
+        System.out.println(obj);
+    }
+
+
 }
+
+
+
+
+}
+
+
